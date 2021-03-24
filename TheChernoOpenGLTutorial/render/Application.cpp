a5d8b9ab43af99ff0b35f8f4680ab59ecca24737
zhulinspace <zhulin19970115@gmail.com>
2021-03-24 09:47:25 +0800

    add assimp module

--- TheChernoOpenGLTutorial/render/Application.cpp
+++ TheChernoOpenGLTutorial/render/Application.cpp
@@ -14,15 +14,35 @@
 
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtx/string_cast.hpp"
 
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 
-#include "Camera.h"
+//#include "Camera.h"
+//#include<DbgHelp.h>
 
-#include <ft2build.h>
-#include FT_FREETYPE_H  
+const unsigned int ScreenWidth = 1280;
+const unsigned int ScreenHeight = 960;
+//float lastX = ScreenWidth / 2.0f;
+//float lastY = ScreenHeight / 2.0f;
+//bool firstMouse = true;
+//void mouse_callback(GLFWwindow *window, double xpos, double ypos)
+//{
+//	if (firstMouse)
+//	{
+//		lastX = xpos;
+//		lastY = ypos;
+//		firstMouse = false;
+//	}
+//	float xoffset = xpos - lastX;
+//	float yoffset = lastY - ypos;
+//	lastX = xpos;
+//	lastY = ypos;
+//
+//
+//}
 
 int main(void)
 {
@@ -42,9 +62,8 @@ int main(void)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	/* Create a windowed mode window and its OpenGL context */
-	const unsigned int SrceenWidth = 640;
-	const unsigned int ScreenHeight = 480;
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	
+	window = glfwCreateWindow(ScreenWidth, ScreenHeight, "Hello World", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -125,7 +144,10 @@ int main(void)
 		lightVa.AddBuffer(vb, lightLayout);
 
 		glm::mat4 proj = glm::mat4(1.0f);
-		proj = glm::perspective(glm::radians(45.0f), (float)SrceenWidth / (float)ScreenHeight, 0.1f, 100.0f);
+
+	
+		proj = glm::perspective(glm::radians(45.0f), (float)ScreenWidth / (float)ScreenHeight, 0.1f, 100.0f);
+
 
 		Shader lightShader("res/shaders/light.shader");
 		Shader shader("res/shaders/Basic.shader");
@@ -160,7 +182,13 @@ int main(void)
 		ImGui::StyleColorsDark();
 
 		float angel = 0;
-		glm::vec3 translation(0.0f, 0.0f, -3.0f);
+		glm::vec3 translation(0.0f, 0.0f, 0.0f);
+		glm::vec3 eye(0.0f, 0.0f, 3.0f);
+		glm::vec3 center(0.0f, 0.0f, 0.0f);
+		glm::vec3 up(0.0f, 1.0f, 0.0f);
+
+		//≤‚ ‘ 
+		glm::vec4 test(-0.5f, -0.5f, -0.5f, 1);
 
 		/* Loop until the user closes the window */
 		while (!glfwWindowShouldClose(window))
@@ -176,8 +204,23 @@ int main(void)
 			glm::mat4 view = glm::mat4(1.0f);
 		
 			model = glm::rotate(model, glm::radians(-angel), glm::vec3(0.5f, 1.0f, 0.0f));
-			view = glm::translate(view, translation);
+			view = glm::lookAt(eye, center, up);
+
+			//view = glm::translate(view, translation);
 			
+
+			
+			glm::vec4 m = model * test;
+		
+			glm::vec4 v = view * m;
+			
+			glm::vec4 p = proj * v;
+		
+		
+		
+
+
+
 			glm::vec3 lightPos(0.8f, 0.2f, 2.0f);
 			glm::vec3 ViewPos(0.0f,0.0f,3.0f);
 
@@ -211,7 +254,23 @@ int main(void)
 			{
 				ImGui::SliderFloat("angel", &angel, -180.0f, 180.0f);      
 				ImGui::SliderFloat3("translation", &translation.x,-2.0f,2.0f);
+				ImGui::Text("origin test vector: %s",glm::to_string(test).c_str());
+				ImGui::Text("model matrix: \n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f",   model[0][0], model[1][0], model[2][0], model[3][0], 
+																																							model[0][1], model[1][1], model[2][1], model[3][1], 
+																																							model[0][2], model[1][2], model[2][2], model[3][2],
+																																							model[0][3], model[1][3], model[2][3], model[3][3] );
+				ImGui::Text("after model matrix: %s", glm::to_string(m).c_str());
+				ImGui::Text("view matrix: \n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f",	    view[0][0], view[1][0], view[2][0], view[3][0],
+																																							view[0][1], view[1][1], view[2][1], view[3][1],
+																																							view[0][2], view[1][2], view[2][2], view[3][2],
+																																							view[0][3], view[1][3], view[2][3], view[3][3]);
 
+				ImGui::Text("after view matrix: %s", glm::to_string(v).c_str());
+				ImGui::Text("projection matrix: \n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f",	    proj[0][0], proj[1][0], proj[2][0],proj[3][0],
+																																									proj[0][1], proj[1][1], proj[2][1],proj[3][1],
+																																									proj[0][2], proj[1][2], proj[2][2],proj[3][2],
+																																									proj[0][3], proj[1][3], proj[2][3],proj[3][3]);
+				ImGui::Text("after projection matrix: %s", glm::to_string(p).c_str());
 				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 			}
 
